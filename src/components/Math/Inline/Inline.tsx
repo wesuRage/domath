33f1152fe944455c12a0138790cd20c5f69da487
wesuRage <weslley.nmiranda@gmail.com>
2023-12-29 20:53:34 -0300

    font size corrected

--- src/components/Math/Inline/Inline.tsx
+++ src/components/Math/Inline/Inline.tsx
@@ -1,9 +1,13 @@
+import { Container } from "./styles";
+
 export default function MathInline({ math }: { math: string }) {
   return (
-    <img
-      src={`https://math.vercel.app/?color=red&bgcolor=black&inline=${encodeURIComponent(
-        math
-      )}.svg`}
-    />
+    <Container>
+      <img
+        src={`https://math.vercel.app/?color=red&bgcolor=black&inline=${encodeURIComponent(
+          math
+        )}.svg`}
+      />
+    </Container>
   );
 }
---
+++ src/components/Math/Inline/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: inline;
+
+  @media screen and (max-width: 550px) {
+    img {
+      max-height: 8px;
+    }
+  }
+`;

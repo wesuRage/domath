669372f12c8f6861d57a982029505376aaddb473
wesuRage <weslley.nmiranda@gmail.com>
2023-12-29 23:46:51 -0300

    sidebar update

--- src/app/matematica_basica/fracoes/page.tsx
+++ src/app/matematica_basica/fracoes/page.tsx
@@ -1,14 +1,26 @@
 "use client";
 
 import Main from "@/components/Main/Main";
+import MathInline from "@/components/Math/Inline/Inline";
 import Sidebar from "@/components/Sidebar/Sidebar";
+import Paragraph from "@/components/Text/Paragraph/Paragraph";
+import Title from "@/components/Text/Title/Title";
 import { Wrapper } from "@/components/Wrapper/Wrapper";
 
 export default function Fracoes() {
   return (
     <Wrapper>
       <Sidebar />
-      <Main>oi</Main>
+      <Main>
+        <Title>Frações</Title>
+        <Paragraph>
+          Uma fração representa uma parte de um todo, ou seja, representa um
+          pedaço de algo inteiro. Na sua essência é uma divisão, e sua notação é
+          escrita e lida como: <MathInline math="\frac{1}{2}" />
+          (um meio), <MathInline math="\frac{3}{4}" />
+          (três quartos)
+        </Paragraph>
+      </Main>
     </Wrapper>
   );
 }
--- src/components/Text/Anchor/SidebarAnchor/styles.ts
+++ src/components/Text/Anchor/SidebarAnchor/styles.ts
@@ -2,18 +2,22 @@ import Link from "next/link";
 import styled from "styled-components";
 
 export const Anc = styled(Link)`
-  margin-left: 40px;
+  padding-left: 30px;
+  margin-left: 10px;
   text-decoration: none;
 
   color: var(--text-color);
 
-  display: inline-block;
+  display: block;
 
   &:visited {
     color: var(--text-color);
   }
 
   &:hover {
-    color: var(--main-color);
+    background-color: var(--main-color);
+    color: var(--bg-color);
+    font-weight: bold;
+    border-radius: 10px;
   }
 `;
